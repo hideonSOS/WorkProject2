a36867445d0d87e8ec8a468e4adfea8cb1873925
hideonSOS <code1555@icloud.com>
2022-05-26 17:04:38 +0900

    test

--- WorkApp2/scrapeon.py
+++ WorkApp2/scrapeon.py
@@ -35,51 +35,51 @@ def TestCroll(url,selectoron):
 		start_chrome(url, chromedriver_path, options=options)
 		testlist =[]
 		time.sleep(10)
-		elem = PickUpData(selectoron)
-		dict = {
-		'No':[],
-		'toban':[],
-		'name':[],
-		'rank':[],
-		'average':[],
-		'point':[],
-		'accident':[],
-		'totalrun':[],
-		'result':[],
-		}
-		for i, a in enumerate(elem):
-			# print(str(a))/
-			if 'col1' in str(a):
-				dict['No'].append(pick(str(a)))
-			elif 'col2' in str(a):
-				dict['toban'].append(pick(str(a)))
-			elif 'col3' in str(a):
-				dict['name'].append(pick(str(a)))
-			elif 'col4' in str(a):
-				dict['rank'].append(pick(str(a)))
-			elif 'col5' in str(a):
-				dict['average'].append(pick(str(a)))
-			elif 'col6' in str(a):
-				dict['point'].append(pick(str(a)))
-			elif 'col7' in str(a):
-				dict['accident'].append(pick(str(a)))
-			elif 'col8' in str(a):
-				dict['totalrun'].append(pick(str(a)))
-			elif 'col9' in str(a):
-				dict['result'].append(pick(str(a)))	
-			else:
-				print(f' error >>> {str(a)}')
+		# elem = PickUpData(selectoron)
+		# dict = {
+		# 'No':[],
+		# 'toban':[],
+		# 'name':[],
+		# 'rank':[],
+		# 'average':[],
+		# 'point':[],
+		# 'accident':[],
+		# 'totalrun':[],
+		# 'result':[],
+		# }
+		# for i, a in enumerate(elem):
+		# 	# print(str(a))/
+		# 	if 'col1' in str(a):
+		# 		dict['No'].append(pick(str(a)))
+		# 	elif 'col2' in str(a):
+		# 		dict['toban'].append(pick(str(a)))
+		# 	elif 'col3' in str(a):
+		# 		dict['name'].append(pick(str(a)))
+		# 	elif 'col4' in str(a):
+		# 		dict['rank'].append(pick(str(a)))
+		# 	elif 'col5' in str(a):
+		# 		dict['average'].append(pick(str(a)))
+		# 	elif 'col6' in str(a):
+		# 		dict['point'].append(pick(str(a)))
+		# 	elif 'col7' in str(a):
+		# 		dict['accident'].append(pick(str(a)))
+		# 	elif 'col8' in str(a):
+		# 		dict['totalrun'].append(pick(str(a)))
+		# 	elif 'col9' in str(a):
+		# 		dict['result'].append(pick(str(a)))	
+		# 	else:
+		# 		print(f' error >>> {str(a)}')
 
-		df = pd.DataFrame()
-		df['No']=dict['No']
-		df['toban']=dict['toban']
-		df['name']=dict['name']
-		df['rank']=dict['rank']
-		df['average']=dict['average']
-		df['point']=dict['point']
-		df['accident']=dict['accident']
-		df['totalrun']=dict['totalrun']
-		df['result']=dict['result']
+		# df = pd.DataFrame()
+		# df['No']=dict['No']
+		# df['toban']=dict['toban']
+		# df['name']=dict['name']
+		# df['rank']=dict['rank']
+		# df['average']=dict['average']
+		# df['point']=dict['point']
+		# df['accident']=dict['accident']
+		# df['totalrun']=dict['totalrun']
+		# df['result']=dict['result']
 		
 		kill_browser()
 		time.sleep(10)
@@ -89,7 +89,7 @@ def TestCroll(url,selectoron):
 		print('接続エラー')
 		df = pd.DataFrame()
 		time.sleep(10)
-	return df
+	# return df
 	
 # testelem = TestCroll(url3,selector_str)
 
